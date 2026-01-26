ffa862b674006372420384ba3da5e40e25295246
tlm118 <84851082+tlm118@users.noreply.github.com>
2026-01-26 18:09:02 +0800

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -40,30 +40,6 @@ useHead({
 })
 </script>
 
-<script setup>
-import { ref } from 'vue';
-import axios from 'axios';
-
-const data = ref(null);
-const error = ref(null);
-
-const fetchData = async () => {
-  try {
-    const response = await axios.get('http://doc.7ge.top/', {
-      validateStatus: () => true // 确保axios捕获所有状态码，包括重定向状态码
-    });
-    data.value = response.data;
-  } catch (err) {
-    error.value = err;
-    if (err.response && err.response.status === 307) {
-      // 处理307重定向，例如重新请求新位置
-      window.location.href = err.response.headers.location; // 或者重新发起请求到新的URL
-    }
-  }
-};
-</script>
-
-
 <template>
   <NuxtLayout>
     <NuxtLoadingIndicator color="#000" />
