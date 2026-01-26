46ad799ca39945a5a012c5b811ff3a04127405be
tlm118 <84851082+tlm118@users.noreply.github.com>
2026-01-26 20:43:45 +0800

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,11 @@
 <template>
+  <main class="flex min-h-screen flex-col">
+    <LayoutsHeader />
+    <section class="flex flex-1">
+      <div class="container">
+        <slot />
+      </div>
+    </section>
 
+  </main>
 </template>
